56bc02470aba710ac40430aa8049c224d41ddfd4
bcherrera14 <bcherrera14@gmail.com>
2022-10-22 22:36:57 -0700

    add navbar desktop style

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
-    <div>
+    <div className="appContainer">
       <Router>
         <Routes>
           <Route path='/' element={<Workout/>}/>
