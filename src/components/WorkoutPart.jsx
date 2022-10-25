d0a102d426a74ef4af6062216f6f3a08e26ef013
bcherrera14 <bcherrera14@gmail.com>
2022-10-24 21:03:15 -0700

    add workout to db

--- src/components/WorkoutPart.jsx
+++ src/components/WorkoutPart.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function WorkooutPart({index, onDelete}) {
+function WorkooutPart({index, onDelete, onChange}) {
 
   return (
     <div className="mb-3 ">
         <label className="form-label">Part {index}</label>
         <div className="input-group">
-          <input type="text" className="form-control" id={`part${index}`}/>
-          <span className="input-group-text" id="basic-addon2" onClick={()=>onDelete(index)}><i className="fa-solid fa-circle-minus"></i></span>
+          <input type="text" className="form-control" id={`part${index}`} onChange={onChange} required/>
+          <span className="input-group-text" onClick={()=>onDelete(index)}><i className="fa-solid fa-circle-minus"></i></span>
         </div>
         <div className="form-text">Use comma separated values for new line.</div>
     </div>
--- src/pages/NewWorkout.jsx
+++ src/pages/NewWorkout.jsx
@@ -1,44 +1,97 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import {setDoc, doc, serverTimestamp, addDoc, collection} from 'firebase/firestore'
+import {db} from '../firebase.config'
 import { ReactComponent as AddIcon } from '../assets/circle-plus-solid.svg'
 import WorkoutPart from '../components/WorkoutPart'
+import { toast } from 'react-toastify'
 
 
 function NewWorkout() {
-    const [parts, setParts] = useState([])
+  const [parts, setParts] = useState([])
+  const [formData, setFormData] = useState({
+    date: '',
+    parts: parts.length + 1,
+    part1: ''
+  })
 
-    useEffect(() => {
-        console.log(parts)
-    }, [parts])
+  const {date, part1} = formData
+
+  useEffect(() => {
+      setFormData((prevState) => ({
+        ...prevState,
+        parts: parts.length + 1
+      }))
+  }, [parts])
+  
+  const onClick = () => {
+      const index = parts.length + 1
+      setParts([...parts, index])
+  }
+
+  const onDelete = (index) => {
+      if(index - 1 === parts.length ){
+          console.log('Delete Item')
+          setParts(parts.slice(0,parts.length-1))
+      }
+  }
+
+  const onChange = (e) => {
+    e.preventDefault()
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
     
-    const onClick = () => {
-        const index = parts.length + 1
-        setParts([...parts, index])
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const formDataCopy = {...formData}
+      formDataCopy.timestamp = serverTimestamp()
+
+      //Format Date
+      formDataCopy.date = formDataCopy.date.split('-')
+      const year = formDataCopy.date[0]
+      formDataCopy.date.shift()
+      formDataCopy.date.push(year)
+      formDataCopy.date = formDataCopy.date.join('/')
+      const workoutRef = doc(collection(db, 'workouts'))
+      await setDoc(workoutRef, formDataCopy)
+      toast.success('Workout created.')
+      setParts([])
+      setFormData({
+        date: '',
+        parts: parts.length + 1,
+        part1: ''
+      })
+      
+    } catch (error) {
+      toast.error('Could not create new workout.')
+      console.log(error)
     }
 
-    const onDelete = (index) => {
-        if(index - 1 === parts.length){
-            console.log('Delete Item')
-            setParts(parts.slice(0,parts.length-1))
-        }
-    }
+    
+  }
+
   return (
     <div className='pageContainer'>
       <header>
         <p className='pageHeader'>Create Workout</p>
       </header>
       <main>
-        <form className='d-flex flex-column'>
+        <form onSubmit={onSubmit} className='d-flex flex-column'>
             <div className="mb-3">
                 <label className="form-label">Date</label>
-                <input type="date" className="form-control" id="date"/>
+                <input type="date" className="form-control" id="date" value={date} onChange={onChange} required/>
             </div>
             <div className="mb-3">
                 <label className="form-label">Part 1</label>
-                <input type="text" className="form-control" id="part1"/>
+                <input type="text" className="form-control" id="part1" value={part1} onChange={onChange} required/>
                 <div className="form-text">Use comma separated values for new line.</div>
             </div>
-            {parts.length > 0 && parts.map((index) => <WorkoutPart onDelete={onDelete} index={index + 1} key={index}/>)}
+            {parts.length > 0 && parts.map((index) => <WorkoutPart onDelete={onDelete} index={index + 1} key={index} onChange={onChange}/>)}
             <div className='d-flex align-items-center mb-3' onClick={onClick}>
                 <AddIcon width='20px' height='20px' />
                 <p className='ms-2 mb-0'>Add another part.</p>
